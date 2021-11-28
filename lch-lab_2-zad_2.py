df4df781d8c357dbd8d733795e10b01ab05e9297
laurachela <laura.chela@edu.uekat.pl>
2021-11-28 09:36:50 +0100

    lab_5 PEP 8

--- lch-lab_2-zad_2.py
+++ lch-lab_2-zad_2.py
@@ -7,45 +7,51 @@ Zad. 2
 """
 # a) Utwórz funkcje, która otrzyma w parametrze listę 5 imion, a nastepnie wyświetl każde z nich.
 
+
 def wyswietl_imiona(names: list) -> None:
     for name in names:
         print(name)
 
-wyswietl_imiona(names = ['name1', 'name2' ,'name3' ,'name4' ,'name5'])
+
+wyswietl_imiona(names=['name1', 'name2', 'name3', 'name4', 'name5'])
 
 # b) Utwórz funkcję, która otrzyma w parametrze listę zawierającą 5 dowolnych
 # liczb, każdy jej element pomnoży przez 2, a na końcu ją zwróci.
 # Zadanie należy wykonać w 2 wersjach:
 # i)
 
-def liczbyx2(numbers: list) -> list:
+
+def liczbyx2(tab_numbers: list) -> list:
     lista = []
-    for number in numbers:
-        lista.append(number*2)
+    for i_number in tab_numbers:
+        lista.append(i_number*2)
     return lista
 
-print(liczbyx2(numbers = [1, 2, 3, 4, 5]))
+
+print(liczbyx2(tab_numbers=[1, 2, 3, 4, 5]))
 
 # ii)
 
-def liczbyx2next(numbers: list) -> list:
-    return [number*2 for number in numbers]
 
-print(liczbyx2next(numbers = [1, 2, 3, 4, 5]))
+def liczbyx2next(tab2_numbers: list) -> list:
+    return [i2number*2 for i2number in tab2_numbers]
+
+
+print(liczbyx2next(tab2_numbers=[1, 2, 3, 4, 5]))
 
 
 # c) Utwórz pętlę iterującą po liście zawierającej 10 liczb (rekomendowane
 # wykorzystanie funkcji range ), która wyświetla jedynie parzyste elementy.
 
-numbers = range(1,10)
-for number in numbers:
-    if number%2==0:
-        print(number)
+numbers2 = range(1, 10)
+for x in numbers2:
+    if x % 2 == 0:
+        print(x)
 
-#d) Utwórz pętlę iterującą po liście zawierającej 10 liczb (rekomendowane
+# d) Utwórz pętlę iterującą po liście zawierającej 10 liczb (rekomendowane
 # wykorzystanie funkcji range ), która wyświetla co drugi element.
 
-numbers = range(1,10)
-for i, number in enumerate(numbers):
-    if i%2==0:
-       print(number)
+numbers3 = range(1, 10)
+for i, y in enumerate(numbers3):
+    if i % 2 == 0:
+        print(y)
